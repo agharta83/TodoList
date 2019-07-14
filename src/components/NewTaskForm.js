627f6fd3c2069e9ef71555fb596c19d93f16d086
agharta83 <35310694+agharta83@users.noreply.github.com>
2019-07-14 19:02:27 +0200

    use localstorage with useEffect

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -13,7 +13,7 @@ const NewTaskForm = () => {
 
     return (
         <form id="form" onSubmit={handleSubmit}>
-            <input className="todo-input" type="text" placeholder="Add a task" value={label} onChange={ (e) => setLabel(e.target.value) } />
+            <input className="todo-input" type="text" placeholder="Add a task" value={label} onChange={ (e) => setLabel(e.target.value) } required />
             <input className="todo-submit" type="submit" value="Add" />
         </form>
     );
